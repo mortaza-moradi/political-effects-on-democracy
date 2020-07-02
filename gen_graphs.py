6be42922fd96f79b8b2b3301e91f1a9a06a36de2
Mortaza Moradi <mortaza@Mortazas-MacBook-Pro.local>
2020-07-01 22:38:41 -0700

    added graph on political leaning vs democratic score.

--- gen_graphs.py
+++ gen_graphs.py
@@ -3,7 +3,7 @@ import numpy as np
 import plotly.express as pl
 import plotly.io as pio
 import plotly.graph_objects as go
-from sklearn.metrics import r2_score
+#import statsmodels.formula.api as smf
 #import chart_studio.tools as tls
 #from sklearn.preprocessing import PolynomialFeatures
 
@@ -37,19 +37,21 @@ poly_fit = np.polyfit(np.array(x), np.array(y), 2)
 f = np.poly1d(poly_fit)
 
 #Calculate new expected points
-x_inc = np.linspace(3, 4.7, 200)
+x_inc = np.linspace(3, 4.7, 240)
 y_hat = f(x_inc)
 ##r^2 = .17 :(
-fig_demscoreVSdemderiv.add_trace(go.Scatter(x=x_inc, y=y_hat, name="Trendline", hoverinfo="skip",
+
+fig_demscoreVSdemderiv.add_trace(go.Scatter(x=x_inc, y=y_hat, name="Trendline", hoverinfo="skip", mode="lines",
                                             line={
-                                                'color': '#474747',
-                                                'dash': 'dash',
+                                                'color': '#24470A',
+                                                'dash': 'solid',
+                                                'width': 4
 }))
 
 
 fig_demscoreVSdemderiv.update_traces(marker={
     'size': 10,
-    'color': '#24470A'
+    'color': 'rgba(71, 71, 71, .4)'
     })
 
 fig_demscoreVSdemderiv.update_layout(
@@ -87,3 +89,68 @@ fig_demscoreVSdemderiv.update_layout(
 pio.write_html(fig_demscoreVSdemderiv, file="demscoreVSdemderiv.html")
 
 ############~~~~~~~~~~~~~~~~~~~~~~~~~###############
+
+
+####### SECOND GRAPH (Political Leaning VS Demscore) #######
+
+fig_leanVSdemscore = pl.scatter(x = df["American Political Leaning"], y=df["Democratic Score"])
+
+fig_leanVSdemscore.update_traces(marker={
+    'size': 10,
+    'color': 'rgba(71, 71, 71, .4)'
+    })
+
+## Add linear regression
+# Remove nan lines
+x = np.asarray(df["American Political Leaning"].drop([10, 48]))
+y = np.asarray(df["Democratic Score"].drop([10, 48]))
+
+poly_fit = np.polyfit(np.array(x), np.array(y), 1)
+f = np.poly1d(poly_fit)
+
+#Calculate new expected points
+x_inc = np.linspace(-1.8, 3.8, 8)
+y_hat = f(x_inc)
+
+fig_leanVSdemscore.add_trace(go.Scatter(x=x_inc, y=y_hat, name="TrendLine", hoverinfo="skip", mode="lines", 
+                                        line={
+                                                'color': '#24470A',
+                                                'dash': 'solid',
+                                                'width': 4
+}))
+
+fig_leanVSdemscore.update_layout(
+    template="ggplot2",
+    showlegend=False,
+    title={
+        'text': "American Political Leaning vs Democratic Score",
+        'x': .1,'y': .925
+    },
+    yaxis_title="Democratic Score",
+    xaxis_title="American Political Leaning",
+    plot_bgcolor="rgba(242, 242, 242, 0.75)",
+    yaxis={
+        #Dinstinguish between increasing and decreasing
+        'tickvals': [3.5, 4.5],
+        'tickcolor': '#FFF',
+        'range': [3, 5.5], 'zeroline': False
+    },
+    xaxis={
+        #Only label the range, as values are meaningless to viewer
+        'tickvals': [-1, 0, 1, 2, 3],
+        'range': [-2.4, 4.4],
+        'tickcolor': '#FFF',
+        'showgrid': False, 'zeroline': False
+    },
+    hoverlabel={
+        'font_family': 'Didot',
+        'font_size': 11,
+        'bgcolor': 'rgba(242, 242, 242, 0.75)'
+    },
+    titlefont={'size': 20},
+    font={'family': 'Didot', 'color': 'rgba(40, 40, 40)'}
+)
+
+
+#fig_leanVSdemscore.show()
+pio.write_html(fig_leanVSdemscore, file="leanVSdemscore.html")
