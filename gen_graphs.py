6531ea1f6a7824cfed82ca2bec7ae4122f59247a
Mortaza Moradi <mortaza@Mortazas-MacBook-Pro.local>
2020-07-25 02:49:53 -0700

    first edition

--- gen_graphs.py
+++ gen_graphs.py
@@ -7,24 +7,75 @@ from plotly.subplots import make_subplots
 #import statsmodels.formula.api as smf
 #import chart_studio.tools as tls
 #from sklearn.preprocessing import PolynomialFeatures
+#from sklearn.linear_model import LinearRegression
 
 def import_clean_data():
     #Import data as dataframe
     df = pd.read_csv("_data/LeanValues_New.csv")
     
     #Drop the attributes column, and corresponding empty rows
-    df = df.drop(columns=['Unnamed: 10', 'OVERALL AMERICAN (L) CHANGE', 'OVERALL AMERICAN (C) CHANGE'])
+    df = df.drop(columns=['Unnamed: 11', 'OVERALL AMERICAN (L) CHANGE', 'OVERALL AMERICAN (C) CHANGE'])
     df = df.drop(df.index[49:60])
     
     #Rename columns to be more explanatory
     df = df.rename(columns={'demscore': 'Democratic Score',
                            'demderiv**': 'DemScore Change',
                            'Judicial (-1, +1)*': 'Judicial (-1, +1)',
-                           'demderiv_backwards': 'DemScore Forward Change'})
+                           'demderiv_backwards': 'DemScore Forward Change',
+                           'apl_derivative': 'Political Lean Derivative'})
     
     return df
     
 df = import_clean_data()
+
+
+#Graph democratic scores against the political leaning derivative to see any large scale trends
+fig_leanDerivVSdemscore = go.Figure()
+fig_leanDerivVSdemscore.add_trace(go.Scatter(x=df["Democratic Score"], y=df["Political Lean Derivative"], mode="markers"))
+
+#fig_leanDerivVSdemscore.show()
+
+
+####Make a graph with year vs demscore, its going down but can we see if there are differences between more liberal and less liberal america and its effect on demscore.
+
+fig_yearVSdemscore = go.Figure()
+fig_yearVSdemscore.add_trace(go.Scatter(x=df["Year"], y=df["Democratic Score"], line_shape="spline", connectgaps=True, line={'color': 'rgba(40, 40, 40, .4)', 'width':4}))
+
+fig_yearVSdemscore.update_yaxes(fixedrange=True)
+fig_yearVSdemscore.update_xaxes(fixedrange=True)
+
+fig_yearVSdemscore.update_layout(
+    template="ggplot2",
+    showlegend=False,
+    title={
+        'text': "Democratic Score Over Time",
+        'x': .1,'y': .925
+    },
+    yaxis_title="Democratic Score",
+    xaxis_title="fig a. Year",
+    plot_bgcolor="rgba(242, 242, 242, 0.75)",
+    yaxis={
+        #Dinstinguish between min and max
+        'tickvals': [3.2, 4.5],
+        'range': [3, 4.7],
+        'tickcolor': '#FFF'
+    },
+    xaxis={
+        #Only label the range, as values are meaningless to viewer
+        'tickcolor': '#FFF'
+    },
+    hoverlabel={
+        'font_family': 'Didot',
+        'font_size': 11,
+        'bgcolor': 'rgba(242, 242, 242, 0.75)'
+    },
+    titlefont={'size': 20},
+    font={'family': 'Didot', 'color': 'rgba(40, 40, 40)'}
+)
+
+#fig_yearVSdemscore.show()
+#pio.write_html(fig_yearVSdemscore, 'yearVSdemscore.html')
+
 
 #### FIRST GRAPH (DEM SCORE VS DEM DERIV) ####
 fig_demscoreVSdemderiv = pl.scatter(df, x="Democratic Score", y="DemScore Change")
@@ -46,7 +97,7 @@ y_hat = f(x_inc)
 fig_demscoreVSdemderiv.add_trace(go.Scatter(x=x_inc, y=y_hat, name="Trendline", hoverinfo="skip", mode="lines",
                                             line={
                                                 'color': '#24470A',
-                                                'dash': 'solid',
+                                                'dash': 'dash',
                                                 'width': 4
 }))
 
@@ -64,7 +115,7 @@ fig_demscoreVSdemderiv.update_layout(
         'x': .1,'y': .925
     },
     yaxis_title="Democratic Change",
-    xaxis_title="fig c. Democratic Score",
+    xaxis_title="fig d. Democratic Score",
     plot_bgcolor="rgba(242, 242, 242, 0.75)",
     yaxis={
         #Dinstinguish between increasing and decreasing
@@ -130,7 +181,7 @@ fig_leanVSdemscore.update_layout(
         'x': .1,'y': .925
     },
     yaxis_title="Democratic Score",
-    xaxis_title="fig a. American Political Leaning",
+    xaxis_title="fig b. American Political Leaning",
     plot_bgcolor="rgba(242, 242, 242, 0.75)",
     yaxis={
         #Dinstinguish between increasing and decreasing
@@ -217,9 +268,9 @@ fig_branchesVSdemscore.update_yaxes(title_text="Democratic Scores", fixedrange=T
 fig_branchesVSdemscore.update_yaxes(tickvals=[3.5, 4, 4.5], fixedrange=True, tickcolor="#FFF", tickfont={'color':'#FFF'}, range=[3, 4.8], row=1, col=2)
 fig_branchesVSdemscore.update_yaxes(tickvals=[3.5, 4, 4.5], fixedrange=True, tickcolor="#FFF", tickfont={'color':'#FFF'}, range=[3, 4.8], row=1, col=3)
 
-fig_branchesVSdemscore.update_xaxes(title_text="fig b. Legislative", zeroline=False, fixedrange=True, tickcolor="#FFF", row=1, col=1)
-fig_branchesVSdemscore.update_xaxes(title_text="fig b. Executive", zeroline=False, fixedrange=True, tickcolor="#FFF", row=1, col=2)
-fig_branchesVSdemscore.update_xaxes(title_text="fig b. Judicial", zeroline=False, fixedrange=True, tickcolor="#FFF",row=1, col=3)
+fig_branchesVSdemscore.update_xaxes(title_text="fig c. Legislative", zeroline=False, fixedrange=True, tickcolor="#FFF", row=1, col=1)
+fig_branchesVSdemscore.update_xaxes(title_text="fig c. Executive", zeroline=False, fixedrange=True, tickcolor="#FFF", row=1, col=2)
+fig_branchesVSdemscore.update_xaxes(title_text="fig c. Judicial", zeroline=False, fixedrange=True, tickcolor="#FFF",row=1, col=3)
 
 #fig_branchesVSdemscore.show()
 #pio.write_html(fig_branchesVSdemscore, file="branchesVSdemscore.html")
